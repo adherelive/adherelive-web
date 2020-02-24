4c5b188a1335bc4a0d87eb460e7ea0de8efb02b0
vineet <vineet.sharma@tripock.com>
2020-02-24 13:51:16 +0530

    css updates

--- client/src/Components/Patient/details/index.js
+++ client/src/Components/Patient/details/index.js
@@ -1,13 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { injectIntl } from "react-intl";
 
 class PatientDetails extends Component {
-    render() {
-        const {id = 0} = this.props;
-        return (
-          <div>{id}</div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    const { id = 0 } = this.props;
+    return <div>{id}</div>;
+  }
 }
 
 export default injectIntl(PatientDetails);
