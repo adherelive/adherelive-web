ebb86d72a677e3d13d9b72792e09a33459c12df4
vineet <vineet.sharma@tripock.com>
2020-05-20 10:39:31 +0530

    wip appointments

---
+++ app/controllers/appointments/appointment.controller.js
@@ -0,0 +1,52 @@
+import Controller from "../index";
+import appointmentService from "../../services/appointment/appointment.service";
+
+class AppointmentController extends Controller {
+    constructor() {
+        super();
+    }
+
+    create = async (req, res) => {
+        try {
+            const { body, userDetails } = req;
+            const {
+                participant_two,
+                description,
+                start_date,
+                end_date,
+                organizer
+                // participant_one_type = "",
+                // participant_one_id = "",
+            } = body;
+            const {userId = "10", user : {category = "patient"} = {}} = userDetails || {};
+            const {id: participant_two_id, category : participant_two_type} = participant_two || {};
+
+            const appointment_data = {
+                participant_one_type: category,
+                participant_one_id: userId,
+                participant_two_type,
+                participant_two_id,
+                organizer_type: Object.keys(organizer).length > 0 ? organizer.category : category,
+                organizer_id: Object.keys(organizer).length > 0 ? organizer.id : userId,
+                description,
+                start_date,
+                end_date,
+            };
+
+            const appointment = await appointmentService.addAppointment(appointment_data);
+            console.log("[ APPOINTMENTS ] appointments ", appointment);
+
+            // TODO: schedule event and notifications here
+
+
+            // response
+            return this.raiseSuccess(res, 200, appointment, "appointment created successfully");
+        } catch(error) {
+            console.log("[ APPOINTMENTS ] create error ---> ", error);
+            return this.raiseServerError(res, 500, error, error.getMessage());
+        }
+    }
+
+}
+
+export default new AppointmentController();
--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
-
 import mysql from "../libs/mysql";
+
+import eventRouter from "../routes/api/events";
+import twilioRouter from "../routes/api/twilio";
 
 const app = express();
 
@@ -15,6 +17,9 @@ app.use(
         limit: "50mb"
     })
 );
+
+app.use('/api', eventRouter);
+app.use("/api", twilioRouter);
 
 module.exports = app;
 
