65be9b46dd21ef1691f001efe3718d1f903b3842
vineet <vineet.sharma@tripock.com>
2020-05-29 13:38:12 +0530

    appointment wip

--- app/controllers/appointments/appointment.controller.js
+++ app/controllers/appointments/appointment.controller.js
@@ -12,7 +12,7 @@ class AppointmentController extends Controller {
 
     create = async (req, res) => {
         try {
-            const { body, userDetails } = req;
+            const {body, userDetails} = req;
             const {
                 participant_two,
                 description,
@@ -24,12 +24,12 @@ class AppointmentController extends Controller {
                 // participant_one_id = "",
             } = body;
             console.log("====================> ", userDetails);
-            const {userId, user : {category} = {}} = userDetails || {};
-            const {id: participant_two_id, category : participant_two_type} = participant_two || {};
+            const {userId, user: {category} = {}} = userDetails || {};
+            const {id: participant_two_id, category: participant_two_type} = participant_two || {};
 
             const appointment_data = {
-                participant_one_type: category,
-                participant_one_id: userId,
+                participant_one_type: "doctor",
+                participant_one_id: "1",
                 participant_two_type,
                 participant_two_id,
                 organizer_type: Object.keys(organizer).length > 0 ? organizer.category : category,
@@ -55,11 +55,20 @@ class AppointmentController extends Controller {
             // console.log("[ APPOINTMENTS ] scheduleEvent ", scheduleEvent);
 
             // TODO: schedule event and notifications here
-            await Proxy_Sdk.scheduleEvent({data: eventScheduleData});
+            // await Proxy_Sdk.scheduleEvent({data: eventScheduleData});
 
             // response
-            return this.raiseSuccess(res, 200, appointment, "appointment created successfully");
-        } catch(error) {
+            console.log("000------------- ", appointment[0]);
+            return this.raiseSuccess(res, 200,
+                {
+                    [appointment.id]: {
+                        basic_info: {
+                            ...appointment.getBasicInfo
+                        }
+                    }
+                },
+                "appointment created successfully");
+        } catch (error) {
             console.log("[ APPOINTMENTS ] create error ---> ", error);
             return this.raiseServerError(res, 500, error, error.getMessage());
         }
