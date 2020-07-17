c82833e054d5da229e65cb132787ac2bf9291a40
Vineet Sharma <vineet.sharma@tripock.com>
2020-07-17 11:19:11 +0530

    updated search apis for mobile

---
+++ routes/m-api/college/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+import Authenticate from "../middleware/auth";
+import College from "../../../app/controllers/mControllers/college/college.controller";
+
+router.get(
+    "/",
+    Authenticate,
+    College.getAll
+);
+
+module.exports = router;
---
+++ routes/m-api/severity/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+import Authenticate from "../middleware/auth";
+import Severity from "../../../app/controllers/mControllers/severity/severity.controller";
+
+router.get(
+    "/",
+    Authenticate,
+    Severity.getAll
+);
+
+module.exports = router;
