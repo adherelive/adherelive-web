8c0a718006a76ef0e62df1108158b1749a2e4026
Vineet Sharma <vineet.sharma@tripock.com>
2020-07-21 20:47:45 +0530

    permissions in verifyUser for web doctor

--- app/controllers/user/user.controller.js
+++ app/controllers/user/user.controller.js
@@ -191,12 +191,21 @@ class UserController extends Controller {
 
         const apiUserDetails = await UserWrapper(userData.getBasicInfo);
 
+        let permissions = {
+          permissions: []
+        };
+
+        if (apiUserDetails.isActivated()) {
+          permissions = await apiUserDetails.getPermissions();
+        }
+
         const dataToSend = {
           users: {
             [apiUserDetails.getUserId()]: {
               ...apiUserDetails.getBasicInfo()
             }
           },
+          ...permissions,
           auth_user: apiUserDetails.getUserId(),
           auth_category: apiUserDetails.getCategory()
         };
