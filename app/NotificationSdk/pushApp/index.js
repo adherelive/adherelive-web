d912eaf82ce0d05bf8ccf9718a3dcfb673d57b98
Vineet Sharma <vineet.sharma@tripock.com>
2020-08-19 16:18:28 +0530

    mediine form type update for medication

--- app/NotificationSdk/pushApp/index.js
+++ app/NotificationSdk/pushApp/index.js
@@ -1,7 +1,7 @@
 import EventExecutor from "../executor";
 import Log from "../../../libs/log_new";
 
-Log.filename("NOTIFICATION_SDK > PUSH_APP");
+Log.fileName("NOTIFICATION_SDK > PUSH_APP");
 
 class PushNotification {
     constructor() {
--- app/seeders/20200702185851-test-careplan-template-medication.js
+++ app/seeders/20200702185851-test-careplan-template-medication.js
@@ -18,14 +18,14 @@ module.exports = {
       {
         care_plan_template_id: 1,
         medicine_id: 1,
-        schedule_data: JSON.stringify({ "unit": "mg", "repeat": "weekly", "quantity": 1, "strength": 2, "medicine_id": "1", "repeat_days": ["Mon", "Fri"], "when_to_take": ["6"], "repeat_interval": 0, "medication_stage": "", "duration": 5, description:"Don't eat for 30 minutes after taking this medication" }),
+        schedule_data: JSON.stringify({ "unit": "mg", "repeat": "weekly", "quantity": 1, "strength": 2, "medicine_id": "1", "repeat_days": ["Mon", "Fri"], "when_to_take": ["6"], "repeat_interval": 0, "medication_stage": "", "duration": 5, description:"Don't eat for 30 minutes after taking this medication", medicine_type:"1" }),
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         care_plan_template_id: 1,
         medicine_id: 2,
-        schedule_data: JSON.stringify({ "unit": "ml", "repeat": "weekly", "quantity": 1, "strength": 5, "medicine_id": "3", "repeat_days": ["Sun", "Thu", "Tue"], "when_to_take": ["4"], "repeat_interval": 0, "medication_stage": "", "duration": 6, description:"Take after eating" }),
+        schedule_data: JSON.stringify({ "unit": "ml", "repeat": "weekly", "quantity": 1, "strength": 5, "medicine_id": "3", "repeat_days": ["Sun", "Thu", "Tue"], "when_to_take": ["4"], "repeat_interval": 0, "medication_stage": "", "duration": 6, description:"Take after eating", medicine_type:"2" }),
         created_at: new Date(),
         updated_at: new Date(),
       },
