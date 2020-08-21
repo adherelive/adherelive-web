0e993d89ac651686427a437f8a0957b1fe0cc9fa
Vineet Sharma <vineet.sharma@tripock.com>
2020-08-21 14:28:27 +0530

    template nae check validation fix

--- routes/api/carePlans/validator.js
+++ routes/api/carePlans/validator.js
@@ -7,10 +7,14 @@ const templateCreateCarePlanForm = Joi.object().keys({
     treatment_id: Joi.number().required().label("Incorrect Treatment value selected"),
     severity_id: Joi.number().required().label("Incorrect Severity value selected"),
     condition_id: Joi.number().required().label("Incorrect Condition value selected"),
-    name: Joi.string().trim().required().messages({
-        'string.empty' : "Template name cannot be empty"
+    createTemplate: Joi.boolean(),
+    name: Joi.string().when('createTemplate', {
+        is: Joi.boolean().valid(true),
+        then: Joi.string().trim().required().messages({
+            'string.empty' : "Template name cannot be empty"
+        }),
+        otherwise: Joi.string().optional().allow("")
     }),
-    createTemplate: Joi.boolean()
 });
 
 export const validateCreateCarePlanFromTemplate = (req, res, next) => {
