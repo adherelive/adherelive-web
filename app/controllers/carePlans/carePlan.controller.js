0e4bcc185e1d5eb4ef9960e17742205e7ee63b87
Vineet Sharma <vineet.sharma@tripock.com>
2020-08-21 13:24:07 +0530

    template medication update

--- app/controllers/carePlans/carePlan.controller.js
+++ app/controllers/carePlans/carePlan.controller.js
@@ -199,7 +199,7 @@ class CarePlanController extends Controller {
             for (const medication of medicationsData) {
                 const { schedule_data: { end_date = '', description = "", start_date = '', unit = '', when_to_take = '',
                     repeat = '', quantity = '', repeat_days = [], strength = '', start_time = '', repeat_interval = '', medication_stage = '' } = {},
-                    medicine_id = '' } = medication;
+                    medicine_id = '', medicine_type = "1" } = medication;
                 const dataToSave = {
                     participant_id: patient_id,
                     organizer_type: category,
@@ -209,6 +209,7 @@ class CarePlanController extends Controller {
                     end_date,
                     details: {
                         medicine_id,
+                        medicine_type,
                         start_time: start_time ? start_time : moment(),
                         end_time: start_time ? start_time : moment(),
                         repeat,
@@ -249,6 +250,7 @@ class CarePlanController extends Controller {
                         repeat_days,
                         when_to_take,
                         repeat_interval,
+                        medicine_type,
                         duration: moment(start_date).diff(moment(end_date), "days")
                     }
                 });
