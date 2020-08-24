9a274616ef2b1f655171fa626a96c71fef7bd31c
Vineet Sharma <vineet.sharma@tripock.com>
2020-08-24 11:08:14 +0530

    added medication create reminder

--- app/controllers/medicationReminder/mReminder.controller.js
+++ app/controllers/medicationReminder/mReminder.controller.js
@@ -3,11 +3,13 @@ import moment from "moment";
 import medicationReminderService from "../../services/medicationReminder/mReminder.service";
 import medicineService from "../../services/medicine/medicine.service";
 import carePlanMedicationService from "../../services/carePlanMedication/carePlanMedication.service";
+import doctorService from "../../services/doctor/doctor.service";
 import MedicationWrapper from "../../ApiWrapper/web/medicationReminder";
 import MedicineWrapper from "../../ApiWrapper/web/medicine";
 import carePlanService from "../../services/carePlan/carePlan.service";
 import CarePlanWrapper from "../../ApiWrapper/web/carePlan";
 import PatientWrapper from "../../ApiWrapper/web/patient";
+import DoctorWrapper from "../../ApiWrapper/web/doctor";
 import {
   CUSTOM_REPEAT_OPTIONS,
   DAYS,
@@ -17,12 +19,15 @@ import {
   EVENT_TYPE,
   MEDICATION_TIMING,
   REPEAT_TYPE,
-  MEDICINE_FORM_TYPE
+  MEDICINE_FORM_TYPE, USER_CATEGORY
 } from "../../../constant";
 import Log from "../../../libs/log";
 import {getCarePlanAppointmentIds,getCarePlanMedicationIds,getCarePlanSeverityDetails} from '../carePlans/carePlanHelper'
 import {RRule} from "rrule";
 import EventSchedule from "../../eventSchedules";
+import MedicationJob from "../../JobSdk/Medications/observer";
+import NotificationSdk from "../../NotificationSdk";
+
 
 const FILE_NAME = "WEB - MEDICATION REMINDER CONTROLLER";
 const Logger = new Log(FILE_NAME);
@@ -264,15 +269,29 @@ class MReminderController extends Controller {
 
       // to update later
       const patient = await PatientWrapper(null, patient_id);
+
+      let categoryData = null;
+      if(category === USER_CATEGORY.DOCTOR) {
+          const doctor = await doctorService.getDoctorByData({user_id: userId});
+          categoryData = await DoctorWrapper(doctor);
+      }
+
       const eventData = {
         participants: [userId, patient.getUserId()],
         actor: {
           id: userId,
           details: {
-
+            name: categoryData.getName(),
+            category
           }
-        }
+        },
+        medicationId: mReminderDetails.get("id")
       };
+
+      const medicationJob = MedicationJob.execute(EVENT_STATUS.SCHEDULED, eventData);
+      await NotificationSdk.execute(medicationJob);
+
+      Logger.debug("medicationJob ---> ", medicationJob.getInAppTemplate());
 
       return this.raiseSuccess(
         res,
