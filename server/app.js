68b851d9fdb61152b0fca06835eb28620c2c1356
Vineet Sharma <vineet.sharma@tripock.com>
2020-08-26 17:45:21 +0530

    commented cron part of code

--- server/app.js
+++ server/app.js
@@ -22,9 +22,9 @@ const cors = require("cors");
 
 const app = express();
 
-const cron = schedule.scheduleJob("*/1 * * * *", async () => {
-    await getPriorEvents();
-});
+// const cron = schedule.scheduleJob("*/1 * * * *", async () => {
+//     await getPriorEvents();
+// });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
