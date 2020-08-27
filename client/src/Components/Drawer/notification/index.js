1dc99c0f3e4cd28518a601d51f599b84fc95fd57
Nishchay <nishchay.chaudhary@tripock.com>
2020-08-27 14:30:00 +0530

    mobile notifications wip

--- client/src/Components/Drawer/notification/index.js
+++ client/src/Components/Drawer/notification/index.js
@@ -5,10 +5,13 @@ import config from "../../../config";
 // import Cookies from 'js-cookie';
 import { get, set } from "js-cookie";
 // import { encode, decode } from 'js-base64';
+import moment from 'moment';
 import { connect } from "getstream";
 import messages from './message';
 import "react-datepicker/dist/react-datepicker.css";
 const { Option } = Select;
+const APPOINTMENT = 'appointment';
+const MEDICATION = 'medication';
 
 const { GETSTREAM_API_KEY, GETSTREAM_APP_ID } = config;
 
@@ -84,7 +87,7 @@ class NotificationDrawer extends Component {
             activityGroupId: groupId
         });
         console.log('activities765863563865356886', data, activities);
-        getNotification({activities});
+        getNotification({ activities });
     }
 
     readNotification = (groupId, activity_id) => {
@@ -125,6 +128,62 @@ class NotificationDrawer extends Component {
         }
     };
 
+    handlePatientDetailsRedirect = (patient_id) => () => {
+        const { history, close } = this.props;
+        console.log('12432412351324513245============>',patient_id);
+        history.push(`/patients/${patient_id}`);
+        close();
+
+    };
+
+    renderNotifications = () => {
+        const { notifications = {}, medicines = {}, appointments = {}, medications = {}, patients = {} } = this.props;
+        return (
+            <div className='wp100 flex direction-column'>
+                {Object.values(notifications).map((notification) => {
+                    const { type = '', foreign_id = '' } = notification || {};
+                    let title = '';
+                    let patientName = '';
+                    let time = '';
+                    let purpose = '';
+                    let medicine = '';
+                    let patient_id = '';
+                    if (type === APPOINTMENT) {
+                        const { basic_info: { start_time = '', details: { reason = '' } = {} } = {}, participant_two: { id: patId = '' } = {} } = appointments[foreign_id] || {};
+                        const { basic_info: { first_name = "",
+                            middle_name = '',
+                            last_name = "" } = {}
+                        } = patients[patId] || {};
+                        patient_id = patId;
+                        title = this.formatMessage(messages.appointmentHeading);
+                        patientName = `${first_name} ${middle_name ? `${middle_name} ` : ""}${last_name}`;
+                        time = moment(start_time).format('ll');
+                        purpose = reason;
+                    } else if (type === MEDICATION) {
+                        title = this.formatMessage(messages.medicationHeading);
+                        const { basic_info: { details: { medicine_id = '1' } = {} }, participant_id = '' } = medications[foreign_id] || {};
+                        const { basic_info: { first_name = "",
+                            middle_name = '',
+                            last_name = "" } = {}
+                        } = patients[participant_id] || {};
+                        patient_id = participant_id;
+                        const { basic_info: { name: medName = '' } } = medicines[medicine_id] || {};
+                        medicine = medName;
+                        patientName = `${first_name} ${middle_name ? `${middle_name} ` : ""}${last_name}`;
+                    }
+                    return (
+                        <div className='drawer-block pointer' onClick={this.handlePatientDetailsRedirect(patient_id)}>
+                            <div className='fs16 medium'>{title}</div>
+                            <div className='fs14'>{patientName}</div>
+                            <div className='fs14'>{type === APPOINTMENT ? purpose : medicine}</div>
+                            <div className='fs14'>{time}</div>
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
+
 
     formatMessage = data => this.props.intl.formatMessage(data);
 
@@ -153,6 +212,7 @@ class NotificationDrawer extends Component {
                     visible={visible} // todo: change as per state, -- WIP --
                     width={400}
                 >
+                    {this.renderNotifications()}
                     {/* <div className='add-patient-footer'>
                         <Button onClick={this.onClose} style={{ marginRight: 8 }}>
                             {this.formatMessage(messages.cancel)}
