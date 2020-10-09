89372c2fce3b58be3ab4974d69b87bceb5e8b91c
Jyotika <jyotika.purohit@tripock.com>
2020-10-09 13:00:12 +0530

    add qty option for ml uni in medication reminder

--- client/src/Helper/timeZone.js
+++
@@ -1,6 +0,0 @@
-import moment from "moment";
-const momentObj = moment;
-export const setTimeZone = (timeZone = "Asia/Kolkata") => {
-  momentObj.tz.setDefault(timeZone);
-};
-// export default momentObj;
