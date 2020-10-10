d7a1469bdc9312751f65d6fb123461df1b22b71f
vineet <vineet.sharma@tripock.com>
2020-10-10 12:49:58 +0530

    twilio room id helper

---
+++ client/src/Helper/twilio/index.js
@@ -0,0 +1,6 @@
+import config from "../../config";
+
+export const getRoomId = (doctor, patient) => {
+  console.log("21380138012 config", config, process.env);
+  return`${doctor}-${config.CHANNEL_SERVER}-${patient}`;
+};
