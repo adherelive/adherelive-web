081319025184ff71c635760e6280c039ed89de16
shivang.upadhyay <shivang.upadhyay@tripock.com>
2020-12-02 14:35:44 +0530

    desc: added more permission types for providers

--- app/seeders/20200713083045-user-category-permissions.js
+++ app/seeders/20200713083045-user-category-permissions.js
@@ -155,6 +155,30 @@ module.exports = {
         permission_id: "10",
         created_at: new Date(),
         updated_at: new Date()
+      },
+      {
+        category: USER_CATEGORY.PROVIDER,
+        permission_id: "11",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        category: USER_CATEGORY.PROVIDER,
+        permission_id: "12",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        category: USER_CATEGORY.PROVIDER,
+        permission_id: "13",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        category: USER_CATEGORY.PROVIDER,
+        permission_id: "14",
+        created_at: new Date(),
+        updated_at: new Date()
       }
     ]);
   },
