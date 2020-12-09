5646842680353e398de6357c2bcb384ee41cde14
vineet <vineet.sharma@tripock.com>
2020-12-09 17:46:53 +0530

    seeder update wip

--- app/seeders/20200619222246-create-careplan.js
+++ app/seeders/20200619222246-create-careplan.js
@@ -19,6 +19,57 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
       },
+      {
+        patient_id: 2,
+        doctor_id: 1,
+        care_plan_template_id: null,
+        details: JSON.stringify({
+          treatment_id: 2,
+        }),
+        activated_on: new Date(),
+        renew_on: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        patient_id: 3,
+        doctor_id: 2,
+        care_plan_template_id: null,
+        details: JSON.stringify({
+          treatment_id: 1,
+          severity_id: 2,
+          condition_id: 5
+        }),
+        activated_on: new Date(),
+        renew_on: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        patient_id: 1,
+        doctor_id: 2,
+        care_plan_template_id: null,
+        details: JSON.stringify({
+          treatment_id: 3,
+        }),
+        activated_on: new Date(),
+        renew_on: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        patient_id: 4,
+        doctor_id: 1,
+        care_plan_template_id: null,
+        details: JSON.stringify({
+          treatment_id: 1,
+          condition_id: 7
+        }),
+        activated_on: new Date(),
+        renew_on: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
     ]);
   },
 
