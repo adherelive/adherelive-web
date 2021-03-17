86b568fb8fd644e087d117548786c334b7e18b1a
Jyotika <jyotika.purohit@tripock.com>
2021-03-17 18:19:41 +0530

    changes to test video call comp container files

---
+++ client/src/Components/ChatFullScreen/testAgoraVideo.js
@@ -0,0 +1,350 @@
+import React, { Component } from "react";
+import { injectIntl } from "react-intl";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+import { getDoctorFromRoomId, getPatientFromRoomId } from "../../Helper/twilio";
+
+import config from "../../config";
+import AudioIcon from "../../Assets/images/ico-vc-audio.png";
+import AudioDisabledIcon from "../../Assets/images/ico-vc-audio-off.png";
+import VideoIcon from "../../Assets/images/ico-vc-video.png";
+import VideoDisabledIcon from "../../Assets/images/ico-vc-video-off.png";
+import UserDpPlaceholder from "../../Assets/images/ico-placeholder-userdp.svg";
+import { USER_CATEGORY } from "../../constant";
+import messages from "./messages";
+import Loading from "../Common/Loading";
+import Tooltip from "antd/es/tooltip";
+import { Button,message,Progress } from "antd";
+
+import { getPatientConsultingVideoUrl } from "../../Helper/url/patients";
+import { getRoomId } from "../../Helper/twilio";
+
+
+class TestAgoraVideo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.rtc = {
+      client: null,
+      localAudioTrack: null,
+      localVideoTrack: null
+    };
+
+    this.state = {
+      loading: false,
+      isVideoOn: true,
+      isAudioOn: true,
+      testing:false,
+      audioLevel:0,
+      isVideoOn:false,
+      isAudioOn:false,
+      roomId:null
+    };
+  }
+
+  componentDidMount() {
+
+    this.testDevices();
+    
+  }
+
+  componentDidUpdate(prevProps,prevState){
+  }
+
+//   componentWillUnmount() {
+//     this.rtc.client.removeAllListeners();
+//   }
+
+ 
+
+ 
+    
+  formatMessage = (message, data) =>
+    this.props.intl.formatMessage(message, data);
+
+
+  toggleVideo = async () => {
+    const { isVideoOn } = this.state;
+    await this.rtc.localVideoTrack.setEnabled(!isVideoOn);
+    this.setState({ isVideoOn: !isVideoOn });
+  };
+
+  toggleAudio = async () => {
+    const { isAudioOn } = this.state;
+    const temp = await this.rtc;
+    // console.log("642354754236 ==============>>>>",{RTC:temp, audioTrack: this.rtc.localAudioTrack})
+    await this.rtc.localAudioTrack.setEnabled(!isAudioOn);
+    this.setState({ isAudioOn: !isAudioOn });
+  };
+
+ 
+
+  getVideoButtons = () => {
+    const {  isVideoOn } = this.state;
+    const { toggleVideo, formatMessage } = this;
+    // console.log("642354754236 ####################");
+   
+    return (
+      <div className="ml24 z999 pointer">
+        {isVideoOn ? (
+            <Tooltip title={formatMessage(messages.disableVideo)} placement={"top"}>
+          <img src={VideoIcon} onClick={toggleVideo} alt="chatIcon" />
+            </Tooltip>
+        ) : (
+            <Tooltip title={formatMessage(messages.enableVideo)} placement={"top"}>
+          <img src={VideoDisabledIcon} onClick={toggleVideo} alt="chatIcon" />
+            </Tooltip>
+        )}
+      </div>
+    );
+  };
+
+  openVideoChatTab = roomId => () => {
+
+
+    window.open(
+      `${config.WEB_URL}${getPatientConsultingVideoUrl(roomId)}`,
+      "_self"
+    );
+  };
+
+  getAudioButtons = () => {
+    const {  isAudioOn } = this.state;
+    const { toggleAudio, formatMessage } = this;
+    
+
+    return (
+      <div className="z999 pointer">
+        {isAudioOn ? (
+            <Tooltip title={formatMessage(messages.muteAudio)} placement={"top"} >
+                <img src={AudioIcon} onClick={toggleAudio} alt="chatIcon" />
+            </Tooltip>
+              
+        ) : (
+            <Tooltip title={formatMessage(messages.unMuteAudio)} placement={"top"}>
+                <img src={AudioDisabledIcon} onClick={toggleAudio} alt="chatIcon" />
+            </Tooltip>
+        
+        )}
+      </div>
+    );
+  };
+
+  getStartCallButton = () => {
+      const {roomId=null}=this.state;
+      const {startVideoCall , formatMessage , openVideoChatTab} = this;
+      
+      return (
+        <Button
+        type={"primary"}
+        className={"mb40"}
+        onClick={openVideoChatTab(roomId)}
+      >
+        {formatMessage(messages.startCall)}
+      </Button>
+      )
+  }
+
+  getVideoParticipants = () => {
+    const {
+      match: { params: { room_id } = {} } = {},
+      doctors,
+      patients,
+      auth: { authenticated_category } = {}
+    } = this.props;
+
+    const patientUserId = getPatientFromRoomId(room_id);
+    const doctorUserId = getDoctorFromRoomId(room_id);
+    const roomId = getRoomId(doctorUserId, patientUserId);
+    
+
+    let remoteData = {};
+    let selfData = {};
+
+    // selfUid
+    if (authenticated_category === USER_CATEGORY.DOCTOR) {
+      Object.keys(doctors).forEach(id => {
+        const { basic_info: { user_id } = {} } = doctors[id] || {};
+        if (`${user_id}` === doctorUserId) {
+          selfData = doctors[id] || {};
+        }
+      });
+
+      Object.keys(patients).forEach(id => {
+        const { basic_info: { user_id } = {} } = patients[id] || {};
+        if (`${user_id}` === patientUserId) {
+          remoteData = patients[id] || {};
+        }
+      });
+    } else {
+      // todo: to modify and refractor based on future requirements
+    }
+    if(roomId === null){
+        this.setState({roomId});
+    }
+    return { remoteData, selfData };
+  };
+
+ 
+
+  testDevices = async () => {
+
+    this.setState({testing:true});
+    
+    AgoraRTC.getDevices()
+    .then(async devices => {
+      const audioDevices = devices.filter(function(device){
+          return device.kind === "audioinput";
+      });
+      const videoDevices = devices.filter(function(device){
+          return device.kind === "videoinput";
+      });
+  
+      var selectedMicrophoneId = audioDevices[0].deviceId;
+      var selectedCameraId = videoDevices[0].deviceId;
+      return await Promise.all([
+        AgoraRTC.createCameraVideoTrack({ cameraId: selectedCameraId }),
+        AgoraRTC.createMicrophoneAudioTrack({ microphoneId: selectedMicrophoneId }),
+      ]);
+    })
+    .then(([videoTrack, audioTrack]) => {
+      this.rtc.localVideoTrack = videoTrack;
+      this.rtc.localAudioTrack = audioTrack;
+      console.log("37468236483274",{audioTrack});
+      videoTrack.play("test-container");
+      this.setState({isAudioOn:true,isVideoOn:true});
+      setInterval(() => {
+        const level = audioTrack.getVolumeLevel();
+        console.log("642354754236 local stream audio level", level);
+        const vol = level*1000;
+        this.setState({audioLevel:vol});
+        
+      }, 1000);
+    });
+  }
+
+  render() {
+    const {loading , testing=false , audioLevel=0 , isVideoOn=false , isAudioOn=false } = this.state;
+    const {
+      getVideoParticipants,
+      formatMessage,
+      getVideoButtons,
+      getAudioButtons,
+    } = this;
+
+    const {
+      remoteData: {
+        basic_info: { full_name } = {},
+        details: { profile_pic } = {}
+      } = {}
+    } = getVideoParticipants();
+
+    // console.log("642354754236 =============>>>",{audioLevel});
+
+
+    return (
+      <div className="wp100 hp100">
+
+        <div className="flex direction-row  justify-center wp100 mt40 ">
+              <div className="fs24 fw800 flex align-start wp78 " >
+                  {this.formatMessage(messages.checkAudioVideo)}
+              </div>
+        </div>
+
+        {/*   REMOTE VIEW   */}
+        <div  className="wp100 hp100 flex directon-row algin-center justify-space-between">
+          {loading && (
+            <div className="hp100 wp100 flex direction-column align-center justify-center z1">
+              <Loading className={"wp100"} />
+            </div>
+          )}
+          <div className=" hp100 wp70 flex direction-column align-center justify-center" >
+                <div className="hp50 wp70 relative" id="test-container" >
+
+                    {!isVideoOn
+                    ?
+                    (
+                      <div className="absolute z999 t10 l10 text-white wp100 hp100 flex direction-column align-center justify-center" >
+                        <span className="fs16 fw800" >Video is Disabled</span>
+                      </div>
+                    )
+                    :
+                    null
+                    }
+                    <div className="absolute b10 wp100 flex justify-center ">
+                        <div className="flex align-center justify-center wp100" >
+                            <div className="flex z999" >
+                                {/*   AUDIO   */}
+                                {getAudioButtons()}
+
+                                {/*   VIDEO   */}
+                                {getVideoButtons()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+                <div className="flex direction-row  justify-center wp100 mt40 ">
+                    <div className="fs16 fw800 flex align-start wp70 " >
+                      {this.formatMessage(messages.audioCheck)}
+                      {!isAudioOn
+                      &&
+                      <span className="red ml24" >
+                         {`Audio is off`}
+                      </span>
+                    }
+                    </div>
+                    
+                </div>
+
+                <div className="flex direction-row align-center justify-center wp100 mt28" > 
+                    
+                    <div className="wp70" >
+                        <Progress  
+                        strokeLinecap="square"
+                        percent={audioLevel}
+                        status="active" 
+                        showInfo={false} 
+                        strokeWidth={10}
+                        strokeColor={{
+                            from: '#108ee9',
+                            to: '#87d068',
+                        }}
+                        />
+                    </div>
+                </div>    
+        </div>
+           
+        <div className="wp30 hp100  flex direction-column align-center justify-center" >
+                <div className="flex direction-column" >
+                    <div className="flex direction-column align-center" >
+                        <img
+                        src={profile_pic || UserDpPlaceholder}
+                        className="pointer h80 w80 br50 "
+                        alt="userDp"
+                        />
+                        <div className="fs16 fw800 mt20">
+                            
+                            <span>
+                                {
+                                `${formatMessage(messages.readyToCall)} ${full_name}`
+                                }
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex direction-row align-center justify-center mt20" > 
+                        {this.getStartCallButton()}
+                    </div>
+                    
+                </div>    
+        </div>
+
+        
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default injectIntl(TestAgoraVideo);
