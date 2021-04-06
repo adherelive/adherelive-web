ce7848260502916eebaa95e11c92fb3e4b87c3d6
Jyotika <jyotika.purohit@tripock.com>
2021-04-06 18:21:37 +0530

    changes to add user-role-id col to careplans

--- app/controllers/adhoc/adhoc.controller.js
+++ app/controllers/adhoc/adhoc.controller.js
@@ -2,13 +2,19 @@
 // services
 import userService from "../../services/user/user.service";
 import userRolesService from "../../services/userRoles/userRoles.service";
+import carePanService from "../../services/carePlan/carePlan.service";
+import userRoleService from "../../services/userRoles/userRoles.service";
 
 // wrappers
 import UserWrapper from "../../ApiWrapper/web/user";
+import CarePlanWrapper from "../../ApiWrapper/web/carePlan";
+import DoctorWrapper from "../../ApiWrapper/web/doctor";
+import UserRoleWrapper from "../../ApiWrapper/web/userRoles";
 
 import { getLinkDetails } from "./adhoc.helper";
 import Controller from "../";
 import Logger from "../../../libs/log";
+import carePlanService from "../../services/carePlan/carePlan.service";
 
 const Log = new Logger("WEB > ADHOC > CONTROLLER");
 
@@ -37,6 +43,28 @@ class AdhocController extends Controller {
                     });
                 }
             }
+
+            // -------- update careplans for user_role_id
+
+            const careplans = await carePanService.getAll();
+            if(careplans && careplans.length){
+                for(let i=0; i< careplans.length; i++) {
+                    const careplan = await CarePlanWrapper(careplans[i]);
+                    const carePlanId = await careplan.getCarePlanId();
+                    const doctorId = careplan.getDoctorId();
+                    const doctor = await DoctorWrapper( null , doctorId );
+                    const userId = await doctor.getUserId();
+                    const userRole = await userRolesService.getFirstUserRole(userId);
+                    if(userRole){
+                        const userRoleWrapper = await UserRoleWrapper(userRole);
+                        const userRoleId = userRoleWrapper.getId();
+                        let carePlanData = { ...careplan , user_role_id: userRoleId };
+                        const careplan = await carePlanService.updateCarePlan(carePlanData,carePlanId);
+                    }
+                }
+
+            }
+
             return this.raiseSuccess(res, 200, {}, "All users migrated successfully to user roles.");
         } catch (error) {
             Log.debug("migrateAllUsersToUserRoles 50 error", error);
