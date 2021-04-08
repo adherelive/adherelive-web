a7e9e34880cde4878e28e154a9dc197da4dccbc3
Jyotika <jyotika.purohit@tripock.com>
2021-04-08 12:19:21 +0530

    changes to add user role id to careplan api wrapper for web and mobile

--- app/ApiWrapper/mobile/carePlan/index.js
+++ app/ApiWrapper/mobile/carePlan/index.js
@@ -23,7 +23,8 @@ class CarePlanWrapper extends BaseCarePlan {
       activated_on,
       renew_on,
       expired_on,
-      care_plan_template_id
+      care_plan_template_id,
+      user_role_id
     } = _data || {};
 
     return {
@@ -32,7 +33,8 @@ class CarePlanWrapper extends BaseCarePlan {
         name,
         doctor_id,
         patient_id,
-        care_plan_template_id
+        care_plan_template_id,
+        user_role_id
       },
       details,
       activated_on,
