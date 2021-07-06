0370268c077929d73df013828041c428ae1239da
Jyotika <jyotika.purohit@tripock.com>
2021-07-06 20:49:19 +0530

    chat noti redirect changes

--- client/src/Components/ChatFullScreen/index.js
+++ client/src/Components/ChatFullScreen/index.js
@@ -61,11 +61,18 @@ class ChatFullScreen extends Component {
 
   }
 
-  componentDidUpdate(prevProps,prevState){
-    const {notification_redirect : {patient_id = null} = {} ,patients = {} , authenticated_user = 1 , doctors = {} , resetNotificationRedirect } =this.props;
+  async componentDidUpdate(prevProps,prevState){
+    const {notification_redirect : {patient_id = null} = {}  , resetNotificationRedirect } =this.props;
     const {notification_redirect : { patient_id : prev_patient_id = null } = {} } = prevProps ; 
-    if(patient_id !== prev_patient_id){
+    if(patient_id && patient_id !== prev_patient_id){
+      await this.handleRedirectUpdate();
+      resetNotificationRedirect();
+    }
+  }
 
+  handleRedirectUpdate = () => {
+      const {notification_redirect : {patient_id = null} = {} ,patients = {} , authenticated_user = 1 , doctors = {} , resetNotificationRedirect } =this.props;
+    
       let doctorUserId = null;
 
       const { basic_info : { user_id : patientUserId= null  } ={} } = patients[patient_id] || {};
@@ -87,10 +94,7 @@ class ChatFullScreen extends Component {
           patientUserId: patientUserId,
           patientId: patient_id
       });
-
-      resetNotificationRedirect();
-
-    }
+ 
   }
 
   updateReplyMessageId = (newId = null) => {
