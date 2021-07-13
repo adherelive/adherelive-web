78dc5e8666fcc90458f333459268d2feac7f8b84
Jyotika <jyotika.purohit@tripock.com>
2021-07-13 19:03:01 +0530

    const changes

--- client/src/constant.js
+++ client/src/constant.js
@@ -1894,3 +1894,34 @@ export const MEAL_TIMINGS = [
   DINNER,
   SLEEP
 ];
+
+export const PATIENT_MEAL_TIMINGS = {
+  [WAKE_UP]: {
+    value: "2020-09-24T08:00:00+05:30",
+    text: "Wake Up"
+  },
+  [BREAKFAST]: {
+    value: "2020-09-24T09:00:00+05:30",
+    text: "Breakfast"
+  },
+  [LUNCH]: {
+    value: "2020-09-24T13:00:00+05:30",
+    text: "Lunch"
+  },
+  [EVENING]: {
+    value: "2020-09-24T16:00:00+05:30",
+    text: "Evening Snacks"
+  },
+  [DINNER]: {
+    value: "2020-09-24T20:00:00+05:30",
+    text: "Dinner"
+  },
+  [SLEEP]: {
+    value: "2020-09-24T23:00:00+05:30",
+    text: "Sleep"
+  },
+  [MID_MORNING]: {
+    value: "2020-09-24T11:00:00+05:30",
+    text: "Mid Morning"
+  },
+};
