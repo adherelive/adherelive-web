cf49f8a2c132a3645d034dc766e72b172bf70b10
Gagneet Singh <gagneet@gmail.com>
2021-10-22 00:27:00 +1100

    Updated Crash Mail to give the server name as Demo

--- app/proxySdk/eventExecutor.js
+++ app/proxySdk/eventExecutor.js
@@ -9,18 +9,17 @@ const Scheduler = require("./scheduler");
 class EventExecutor {
   async sendMail(mailData, scheduledJobId) {
     try {
-
-      console.log('INSIDEEEE SEND MAILLLLLLL ');
+      console.log("INSIDE THE SEND MAIL TRY");
       // let isValid = await validateMailData(mailData);
       let response = await EmailManager.sendEmail(mailData);
 
-      console.log("12931931789223 response ------------> ", response);
+      console.log("INSIDE THE SEND MAIL Object Assign -----> ", response);
       Object.assign(
         mailData,
         response ? { status: "SENT" } : { status: "FAILED" }
       );
 
-      console.log('INSIDEEEE SEND MAILLLLLLL 111111122223333         ',mailData,response);
+      console.log("INSIDE THE SEND MAIL Logger: ", mailData, response);
       let logger = new Logger("email", mailData);
       logger.log();
       // if (scheduledJobId && response) {
@@ -29,8 +28,8 @@ class EventExecutor {
       //   });
       // }
     } catch (err) {
-      console.log('INSIDEEEE SEND MAILLLLLLL ERROR       ', err);
-     // Proxy_Sdk.execute(EVENTS.EMAIL_ERROR, err, "mail_error");
+      console.log("INSIDE THE SEND MAIL ERROR: ", err);
+      // Proxy_Sdk.execute(EVENTS.EMAIL_ERROR, err, "mail_error");
     }
   }
 
