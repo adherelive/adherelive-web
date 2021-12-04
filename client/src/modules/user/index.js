2eeb8ec07971adae06ecc5e2e66c22e119dc8939
Gagneet Singh <gagneet@gmail.com>
2021-12-04 23:28:01 +1100

    Code cleanup and updated to match development branch

--- client/src/modules/user/index.js
+++ client/src/modules/user/index.js
@@ -1,27 +1,27 @@
 const intial_state = {
   23: {
-    first_name: "John",
-    middle_name: "",
-    last_name: "Doe",
-    age: 62,
+    first_name: "Gagneet",
+    middle_name: "B",
+    last_name: "Singh",
+    age: 45,
     gender: "M",
-    phone_number: "+91-6298649934",
-    email_id: "doe.john@gmail.com",
-    patient_id: 12990912,
+    phone_number: "+919810739699",
+    email_id: "gagneet@gmail.com",
+    patient_id: ATD / 001 / 005,
     profile_picture:
       "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
     treatment_details: {
-      treatment_name: "Hip Replacement",
+      treatment_name: "Thyroid Condition",
       treatment_severity: 0,
       treatment_condition: "Trauma",
-      treatment_start_date: "12th March, 2019",
-      treatment_doctor: "John Wick",
+      treatment_start_date: "2nd Decenber, 2020",
+      treatment_doctor: "Sparsh Jaiswal",
       treatment_provider: "Alfred",
     },
     alerts: {
       count: 7,
       new_symptoms: ["Shoulder Pain", "Redness on stitches"],
-      missed_appointment: ["26 Nov, 2019"],
+      missed_appointment: ["05 Dec, 2020"],
     },
   },
 };
