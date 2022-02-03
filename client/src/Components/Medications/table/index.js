f634103a1beade158c3fecc6a230076cb2b8185d
Gagneet Singh <gagneet@gmail.com>
2022-02-03 21:43:30 +1100

    Updated careplan medicines for secondary Doctor to edit

--- client/src/Components/Medications/table/index.js
+++ client/src/Components/Medications/table/index.js
@@ -58,11 +58,9 @@ class MedicationTable extends Component {
     const { basic_info: { user_role_id = null } = {} } = care_plans || {};
     let canViewDetails = true;
     if (
-      !isOtherCarePlan &&
-      user_role_id.toString() === auth_role.toString()
-      // ||
-      // (!isEmpty(care_plans) &&
-      //   care_plans.secondary_doctor_user_role_ids.includes(auth_role) === true)
+      (!isOtherCarePlan && user_role_id.toString() === auth_role.toString()) ||
+      (!isEmpty(care_plans) &&
+        care_plans.secondary_doctor_user_role_ids.includes(auth_role) === true)
     ) {
       canViewDetails = false;
     }
@@ -111,11 +109,9 @@ class MedicationTable extends Component {
     const { basic_info: { user_role_id = null } = {} } = care_plans || {};
     let canViewDetails = true;
     if (
-      !isOtherCarePlan &&
-      user_role_id.toString() === auth_role.toString()
-      // ||
-      // (!isEmpty(care_plans) &&
-      //   care_plans.secondary_doctor_user_role_ids.includes(auth_role) === true)
+      (!isOtherCarePlan && user_role_id.toString() === auth_role.toString()) ||
+      (!isEmpty(care_plans) &&
+        care_plans.secondary_doctor_user_role_ids.includes(auth_role) === true)
     ) {
       canViewDetails = false;
     }
