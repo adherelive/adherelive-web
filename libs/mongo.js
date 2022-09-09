ca4043391dc9374073e273f25189d8922be48262
akshay nagargoje <akshay.nagargoje@myrl.tech>
2022-09-09 11:57:58 +0530

    prod mongo url

--- libs/mongo.js
+++ libs/mongo.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://mongo/adhere", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useFindAndModify: false,
-      // useCreateIndex: true,
-    });
+    await mongoose.connect(
+      "mongodb://adherelive:Q3xbZbp5f7O0AqR4@adherelive.aqaaqxe.mongodb.net/adherecdss",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+        // useCreateIndex: true,
+      }
+    );
     console.log("Successfully Connected with mongo Database");
   } catch (ex) {
     console.log(ex);
