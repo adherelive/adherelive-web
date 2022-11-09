6a9d6f3c1384aa9c45f22a748f208af958968817
Gaurav Sharma <gaurav6421@gmail.com>
2022-11-09 16:08:31 +0530

    api added for m

--- routes/commonmiddlewares/auth.js
+++ routes/commonmiddlewares/auth.js
@@ -2,19 +2,11 @@ import WebAuthenticate from "../api/middleware/auth";
 import MobileAuthenticate from "../m-api/middlewares/auth";
 
 export default async (req, res, next) => {
+  let { m } = req.query;
   try {
-    console.log("In Common MiddleWare auth -1");
-    await WebAuthenticate(req, res, next);
-    console.log("In Common MiddleWare auth -2");
-  } catch (err) {
-    console.log("In Common MiddleWare auth -6");
-    try {
-      console.log("In Common MiddleWare auth -3");
-      await MobileAuthenticate(req, res, next);
-      console.log("In Common MiddleWare auth -4");
-    } catch (ex) {
-      console.log("In Common MiddleWare auth -5");
-      return raiseServerError(res);
-    }
+    if (m) MobileAuthenticate(req, res, next);
+    else WebAuthenticate(req, res, next);
+  } catch (ex) {
+    return raiseServerError(res);
   }
 };
--- routes/commonmiddlewares/doctor.js
+++ routes/commonmiddlewares/doctor.js
@@ -2,19 +2,11 @@ import { isDoctor as isWebDoctor } from "../api/middleware/doctor";
 import { isDoctor as isMDoctor } from "../m-api/middlewares/doctor";
 
 export const isDoctor = (req, res, next) => {
+  let { m } = req.query;
   try {
-    console.log("In Common MiddleWare dr -1");
-    isWebDoctor(req, res, next);
-    console.log("In Common MiddleWare dr -2");
+    if (m) isMDoctor(req, res, next);
+    else isWebDoctor(req, res, next);
   } catch (ex) {
-    console.log("In Common MiddleWare dr -3");
-    try {
-      console.log("In Common MiddleWare dr -4");
-      isMDoctor(req, res, next);
-      console.log("In Common MiddleWare dr -5");
-    } catch (ex) {
-      console.log("In Common MiddleWare dr -6");
-      return raiseServerError(res);
-    }
+    return raiseServerError(res);
   }
 };
