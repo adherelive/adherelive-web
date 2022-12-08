b90068dca594974c887b20231540a348e61a6653
Gaurav Sharma <gaurav6421@gmail.com>
2022-12-08 12:14:46 +0530

    add medicin added.

--- routes/api/medicines/index.js
+++ routes/api/medicines/index.js
@@ -4,13 +4,8 @@ const router = express.Router();
 import Authenticate from "../middleware/auth";
 import Medicine from "../../../app/controllers/medicines/medicine.controller";
 
-router.get(
-  "/",
-  Authenticate,
-  Medicine.getAll
-);
+router.get("/", Authenticate, Medicine.getAll);
 
-
-router.post("/", Authenticate, Medicine.addMedicine);
+router.post("/", Authenticate, Medicine.addMedicineByAdmin);
 
 module.exports = router;
