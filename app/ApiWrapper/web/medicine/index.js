8c9def4226633de1a8051b4a5f5e5685657fd5a7
Gaurav Sharma <gaurav6421@gmail.com>
2022-12-13 14:00:29 +0530

    newcode

--- app/ApiWrapper/web/medicine/index.js
+++ app/ApiWrapper/web/medicine/index.js
@@ -8,8 +8,15 @@ class MedicineWrapper extends BaseMedicine {
 
   getBasicInfo = () => {
     const { _data } = this;
-    const { id, name, type, description, creator_id, public_medicine } =
-      _data || {};
+    const {
+      id,
+      name,
+      type,
+      description,
+      creator_id,
+      details,
+      public_medicine,
+    } = _data || {};
     return {
       basic_info: {
         id,
@@ -17,6 +24,7 @@ class MedicineWrapper extends BaseMedicine {
         type,
         description,
         creator_id,
+        details,
         public_medicine,
       },
     };
