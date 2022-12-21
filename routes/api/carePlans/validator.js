91d95604a1ba8b0fbd0b3be3858467d2363693a6
Gaurav Sharma <gaurav6421@gmail.com>
2022-12-21 08:33:58 +0530

    newcode

--- routes/api/carePlans/validator.js
+++ routes/api/carePlans/validator.js
@@ -8,7 +8,7 @@ const templateCreateCarePlanForm = Joi.object().keys({
   dietData: Joi.array(),
   workoutData: Joi.array(),
   clinical_notes: Joi.string().allow(null, ""),
-  follow_up_advise: Joi.array().allow(null, [], ""),
+  follow_up_advise: Joi.array(),
   treatment_id: Joi.number()
     .required()
     .label("Incorrect Treatment value selected"),
