c1b337c3b61bcabd9209ae17230f7c1707bd05d2
Gaurav Sharma <gaurav6421@gmail.com>
2022-12-28 11:18:38 +0530

    newcode

--- app/controllers/doctors/doctor.controller.js
+++ app/controllers/doctors/doctor.controller.js
@@ -4270,6 +4270,7 @@ class DoctorController extends Controller {
           data = { ...data, details };
           medicinId = baseId;
           console.log({ medicinId });
+          console.log(data);
           try {
             let medicaineUpdate = await medicineService.updateMedicine(
               data,
