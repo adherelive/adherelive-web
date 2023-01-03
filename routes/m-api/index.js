02edc13611a8c4fca6a4925145b397f84ba3c12c
Gaurav Sharma <gaurav6421@gmail.com>
2023-01-03 18:25:29 +0530

    his code added

--- routes/m-api/index.js
+++ routes/m-api/index.js
@@ -64,7 +64,8 @@ router.use(async (req, res, next) => {
 
     if (accessToken) {
       const decodedAccessToken = await jwt.verify(accessToken, secret);
-      const { userRoleId: decodedUserRoleId = null } = decodedAccessToken || {};
+      const { userRoleId: decodedUserRoleId = null, his_id: his_id = null } =
+        decodedAccessToken || {};
       const userRoleDetails = await userRolesService.getSingleUserRoleByData({
         id: decodedUserRoleId,
       });
@@ -76,6 +77,7 @@ router.use(async (req, res, next) => {
       } else {
         req.userDetails = {
           exists: false,
+          his_id,
         };
         next();
         return;
