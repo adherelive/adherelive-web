eaed7e281c7e0d67914e857d0a36e6c392733be9
Gaurav Sharma <gaurav6421@gmail.com>
2023-01-31 15:04:19 +0530

    new code added

--- app/controllers/patients/patients.controller.js
+++ app/controllers/patients/patients.controller.js
@@ -1560,7 +1560,7 @@ class PatientController extends Controller {
 
           const startDate = appointmentWrapper.getStartTime();
           const startDateObj = moment(startDate);
-          const { organizer } = await appointmentWrapper.getBasicInfo();
+          const { organizer, provider_id } = await appointmentWrapper.getBasicInfo();
           const diff = startDateObj.diff(now);
 
           if (diff > 0) {
@@ -1569,7 +1569,7 @@ class PatientController extends Controller {
             }
           }
 
-          const { type, provider_id } = appointmentWrapper.getDetails() || {};
+          const { type } = appointmentWrapper.getDetails() || {};
 
           // if (type !== CONSULTATION) {
           const { type_description = "", radiology_type = "", description = "", reason = "" } =
