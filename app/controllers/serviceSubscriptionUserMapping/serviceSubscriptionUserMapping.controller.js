d132f91cb4e1ada050ee903e31cffee1f76080e1
Gaurav Sharma <gaurav6421@gmail.com>
2023-03-08 13:51:24 +0530

    date issue fixed

--- app/controllers/serviceSubscriptionUserMapping/serviceSubscriptionUserMapping.controller.js
+++ app/controllers/serviceSubscriptionUserMapping/serviceSubscriptionUserMapping.controller.js
@@ -577,7 +577,8 @@ class ServiceSubscriptionUserMappingController extends Controller {
         new ServiceSubscriptionUserMappingService();
 
       if (body.durations) {
-        let expire_date = new Date(req.body.start_date);
+        let expire_date = new Date(req.body.startDate);
+        console.log({ expire_date, startDate })
         expire_date.setMonth(expire_date.getMonth() + req.body.durations);
         body = { ...body, expire_date }
       }
