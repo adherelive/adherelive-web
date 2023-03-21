a8e56df865cbe8f1097893ef8fb59494a35f005f
Gaurav Sharma <gaurav6421@gmail.com>
2023-03-21 15:16:15 +0530

    cron

--- app/Crons/renewTxActivity.js
+++ app/Crons/renewTxActivity.js
@@ -1,42 +1,50 @@
 import serviceSubscribeTx from "../services/serviceSubscribeTranaction/serviceSubscribeTranaction";
-import ServiceSubscriptionUserMapping from "../services/serviceSubscriptionUserMapping/serviceSubscriptionUserMapping.service"
+import ServiceSubscriptionUserMapping from "../services/serviceSubscriptionUserMapping/serviceSubscriptionUserMapping.service";
 import Logger from "../../libs/log";
 import moment from "moment";
+import { Op } from "sequelize";
 const Log = new Logger("CRON > RENEW > SUBSCRIPTION");
 
 class RenewTxActivity {
   runObserver = async () => {
     try {
-        
-        // get all the service subscriptionuser mapping that have next rechage date in next7 days
-        let data = {
-            next_recharge_date: {
-                $lte: moment().add(7, 'days').toDate()
-            },
-            expire_date: {
-                $gt: moment().add(7, 'days').toDate()
-            }
-        }
-        console.log({data})
-        let serviceSubscriptionUserMapping = new ServiceSubscriptionUserMapping()
-        let newTxs = await serviceSubscriptionUserMapping.getAllServiceSubscriptionUserMappingByData(data)
+      // get all the service subscriptionuser mapping that have next rechage date in next7 days
+      let data = {
+        next_recharge_date: {
+          [Op.lte]: moment().add(7, "days").toDate(),
+        },
+        expire_date: {
+          [Op.gt]: moment().add(7, "days").toDate(),
+        },
+      };
+      console.log({ data });
+      let serviceSubscriptionUserMapping = new ServiceSubscriptionUserMapping();
+      let newTxs =
+        await serviceSubscriptionUserMapping.getAllServiceSubscriptionUserMappingByData(
+          data
+        );
 
-        for (let i in newTxs){
-            console.log("====1=1=1=1=1=1=1==1=1=1=1=1=1=1=1=1")
-            console.log(newTxs[i])
-            console.log("====1=1=1=1=1=1=1==1=1=1=1=1=1=1=1=1")
-            console.log("id is here", newTxs[i]["id"])
-            let all_details = serviceSubscribeTx.getAllServiceSubscriptionTx({subscription_user_plan_id:newTxs[i]["id"] })
-            console.log(all_details)
-            console.log("======================================")
-        }
+      for (let i in newTxs) {
+        console.log("====1=1=1=1=1=1=1==1=1=1=1=1=1=1=1=1");
+        console.log(newTxs[i]);
+        console.log("====1=1=1=1=1=1=1==1=1=1=1=1=1=1=1=1");
+        console.log("id is here", newTxs[i]["id"]);
+        let all_details = serviceSubscribeTx.getAllServiceSubscriptionTx({
+          subscription_user_plan_id: newTxs[i]["id"],
+        });
+        console.log(all_details);
+        console.log("======================================");
+      }
 
-        // get all serviceSubscribeTx that have same service_sub_plan_Id
+      // get all serviceSubscribeTx that have same service_sub_plan_Id
 
-
-        console.log("================ txActivities ============ txActivities ================")
-        console.log({ newTxs })
-        console.log("================ txActivities ============ txActivities ================")
+      console.log(
+        "================ txActivities ============ txActivities ================"
+      );
+      console.log({ newTxs });
+      console.log(
+        "================ txActivities ============ txActivities ================"
+      );
     } catch (error) {
       Log.debug("RenewSubscription 500 error", error);
     }
