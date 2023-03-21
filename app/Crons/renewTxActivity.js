7f711d8d11eb8de3dbda523e2f1d148be65bcb9d
Gaurav Sharma <gaurav6421@gmail.com>
2023-03-21 17:02:54 +0530

    inactive status added

--- app/Crons/renewTxActivity.js
+++ app/Crons/renewTxActivity.js
@@ -1,6 +1,6 @@
 import serviceSubscribeTx from "../services/serviceSubscribeTranaction/serviceSubscribeTranaction";
 import ServiceSubscriptionUserMapping from "../services/serviceSubscriptionUserMapping/serviceSubscriptionUserMapping.service";
-import { TABLE_NAME as serviceSubscriptionUserMappingTable } from "../models/serviceSubscriptionUserMapping"
+import { TABLE_NAME as serviceSubscriptionUserMappingTable } from "../models/serviceSubscriptionUserMapping";
 import Logger from "../../libs/log";
 import Database from "../../libs/mysql";
 import { TABLE_NAME as serviceSubscribeTranactionTable } from "../models/serviceSubscribeTranaction";
@@ -9,77 +9,82 @@ import { Op } from "sequelize";
 const Log = new Logger("CRON > RENEW > SUBSCRIPTION");
 
 class RenewTxActivity {
-    runObserver = async () => {
+  runObserver = async () => {
+    console.log(
+      "\n\n\n\n\n\n\n\n\n\n\n\n\n\ntx creatining...\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    );
 
-        console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\ntx creatining...\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    try {
+      // get all the service subscriptionuser mapping that have next rechage date in next7 days
+      let data = {
+        next_recharge_date: {
+          [Op.lte]: moment().add(7, "days").toDate(),
+        },
+        expire_date: {
+          [Op.gt]: moment().add(7, "days").toDate(),
+        },
+      };
+      console.log({ data });
+      let serviceSubscriptionUserMapping = new ServiceSubscriptionUserMapping();
+      let newTxs =
+        await serviceSubscriptionUserMapping.getAllServiceSubscriptionUserMappingByData(
+          data
+        );
 
+      for (let i in newTxs) {
+        let all_details = await serviceSubscribeTx.getAllServiceSubscriptionTx({
+          subscription_user_plan_id: newTxs[i]["id"],
+          is_next_tx_create: false,
+        });
+        const transaction = await Database.initTransaction();
         try {
-            // get all the service subscriptionuser mapping that have next rechage date in next7 days
-            let data = {
-                next_recharge_date: {
-                    [Op.lte]: moment().add(7, "days").toDate(),
-                },
-                expire_date: {
-                    [Op.gt]: moment().add(7, "days").toDate(),
-                },
-            };
-            console.log({ data });
-            let serviceSubscriptionUserMapping = new ServiceSubscriptionUserMapping();
-            let newTxs =
-                await serviceSubscriptionUserMapping.getAllServiceSubscriptionUserMappingByData(
-                    data
-                );
-
-            for (let i in newTxs) {
-
-                let all_details = await serviceSubscribeTx.getAllServiceSubscriptionTx({
-                    subscription_user_plan_id: newTxs[i]["id"],
-                    is_next_tx_create: false,
-                });
-                const transaction = await Database.initTransaction();
-                try {
-                    console.log("updating tx table")
-                    await Database.getModel(serviceSubscribeTranactionTable).update(
-                        { is_next_tx_create: true },
-                        {
-                            where: { id: all_details[0]["id"] },
-                            raw: true,
-                            returning: true,
-                            transaction,
-                        }
-                    );
-                    let {id:myid, ...rest} = all_details[0];
-                    console.log({ ...rest, due_date: new Date() })
-                    const txDetails = { ...rest, due_date: new Date() };
-                    console.log("creating in  tx table")
-                    await Database.getModel(
-                        serviceSubscribeTranactionTable
-                    ).create(txDetails, {
-                        raw: true,
-                        transaction,
-                    });
-                    console.log("updating in  userservicesubmapping")
-                    await Database.getModel(
-                        serviceSubscriptionUserMappingTable
-                    ).update({ next_recharge_date: newTxs[i]["next_recharge_date"] }, {
-                        where: {
-                            id: newTxs[i]["id"],
-                        },
-                        raw: true,
-                        returning: true,
-                        transaction,
-                    });
-                    await transaction.commit();
-                } catch (ex) {
-                    console.log(ex)
-                    await transaction.rollback();
-                }
+          console.log("updating tx table");
+          await Database.getModel(serviceSubscribeTranactionTable).update(
+            { is_next_tx_create: true },
+            {
+              where: { id: all_details[0]["id"] },
+              raw: true,
+              returning: true,
+              transaction,
             }
-        } catch (error) {
-
-            Log.debug("RenewSubscription 500 error", error);
+          );
+          let { id: myid, ...rest } = all_details[0];
+          console.log({ ...rest, due_date: new Date() });
+          const txDetails = {
+            ...rest,
+            due_date: new Date(),
+            patient_status: "inactive",
+          };
+          console.log("creating in  tx table");
+          await Database.getModel(serviceSubscribeTranactionTable).create(
+            txDetails,
+            {
+              raw: true,
+              transaction,
+            }
+          );
+          console.log("updating in  userservicesubmapping");
+          await Database.getModel(serviceSubscriptionUserMappingTable).update(
+            { next_recharge_date: newTxs[i]["next_recharge_date"] },
+            {
+              where: {
+                id: newTxs[i]["id"],
+              },
+              raw: true,
+              returning: true,
+              transaction,
+            }
+          );
+          await transaction.commit();
+        } catch (ex) {
+          console.log(ex);
+          await transaction.rollback();
         }
-    };
+      }
+    } catch (error) {
+      Log.debug("RenewSubscription 500 error", error);
+    }
+  };
 }
 
 export default new RenewTxActivity();
