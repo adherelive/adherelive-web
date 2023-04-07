1ab18f6a355f311776de1aca207fa8c6e9597ed2
Gaurav Sharma <gaurav6421@gmail.com>
2023-04-07 14:06:06 +0530

    search

--- migrations/20211230040524-add-column-his.js
+++ migrations/20211230040524-add-column-his.js
@@ -4,11 +4,11 @@ import { TABLE_NAME } from "../app/models/users";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(TABLE_NAME, "his_id", {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      defaultValue: null,
-    });
+    // return queryInterface.addColumn(TABLE_NAME, "his_id", {
+    //   type: Sequelize.INTEGER,
+    //   allowNull: true,
+    //   defaultValue: null,
+    // });
   },
 
   down: (queryInterface, Sequelize) => {
