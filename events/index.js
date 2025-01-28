1b612807a32e4061ce91553093b1d7c183a24a07
Gagneet Singh <gagneet@gmail.com>
2025-01-28 11:51:48 +1100

    Optimized the SQS events code, run timings increased to 10 mins

--- events/index.js
+++ events/index.js
@@ -1,5 +1,6 @@
-import Log from "../libs/log";
 import schedule from "node-schedule";
+import Log from "../libs/log";
+
 import queueService from "../app/services/awsQueue/queue.service";
 
 const Logger = new Log("EVENT SCHEDULE CREATOR");
@@ -7,7 +8,6 @@ const Logger = new Log("EVENT SCHEDULE CREATOR");
 // FOR TEST...
 // const Config = require("../config/config");
 // Config();
-
 // const cron = schedule.scheduleJob("*/1 * * * * *", async () => {
 //     const QueueService = new queueService();
 //     QueueService.receiveMessage("test_queue").then(response => {
@@ -15,20 +15,33 @@ const Logger = new Log("EVENT SCHEDULE CREATOR");
 //     });
 // });
 
+// Added more error handling and made the function simpler
 const SqsObserver = import("./sqsObserver")
-  .then((module) => {
-    const sqs = new module.default();
-    const QueueService = new queueService();
-    const cron = schedule.scheduleJob("*/30 * * * * *", async () => {
-      await sqs.observe(QueueService);
-    });
-  })
-  .catch((err) => {
-    console.log("dynamic import err", err);
-  });
+    .then((module) => {
+        const sqs = new module.default();
+        const QueueService = new queueService();
+        let isProcessing = false; // Flag to track if a process is already running
 
-// while(true) {
-//     (async () => {
-//         await SqsObserver.observe(queueService);
-//     })();
-// }
+        // TODO: Check if changing the SQS timing to 10 mins from 30 seconds helps in the performance
+        const cron = schedule.scheduleJob("0 */10 * * * *", async () => {
+            if (isProcessing) {
+                console.warn("Previous SQS processing still in progress. Skipping this run.");
+                return;
+            }
+
+            isProcessing = true;
+            try {
+                await sqs.observe(QueueService);
+            } catch (error) {
+                console.error("Error processing SQS messages:", error);
+                // Implement exponential backoff or other retry strategies here
+                // For example:
+                // await new Promise(resolve => setTimeout(resolve, 1000 * 2)); // Retry after 2 seconds
+            } finally {
+                isProcessing = false;
+            }
+        });
+    })
+    .catch((err) => {
+        console.error("Dynamic import error in Events: ", err);
+    });
