e3b61a7dd418b09ed7953bb211fbafed3add57b8
Gagneet Singh <gagneet@gmail.com>
2025-02-03 18:17:29 +1100

    MIddleware had debug logs, commented those

--- routes/commonmiddlewares/doctor.js
+++ routes/commonmiddlewares/doctor.js
@@ -1,9 +1,21 @@
 import { isDoctor as isWebDoctor } from "../api/middleware/doctor";
 import { isDoctor as isMDoctor } from "../m-api/middlewares/doctor";
+import {raiseServerError} from "../api/helper";
+import { createLogger } from "../../libs/log";
 
+const log = createLogger("MIDDLEWARE > DOCTOR");
+
+/**
+ *
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
 export const isDoctor = (req, res, next) => {
   let { m } = req.query;
-  log.debug("In the Doctor middle ware ---> request query (m): ", { m });
+  // log.debug("In the Doctor middle ware -> request query (m): ", { m });
 
   try {
     if (m) isMDoctor(req, res, next);
